f660ed354a72cac7869c47d2b21a34b3d6a13eea
Aslan Aliev <aslan.a.aliev@gmail.com>
2022-04-03 04:29:58 +0300

    fix: change French greeting

--- hello.go
+++ hello.go
@@ -6,7 +6,7 @@ const spanish = "Spanish"
 const french = "French"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
+const frenchHelloPrefix = "Bonjour, monsieur "
 
 func Hello(name string, language string) string {
 	if name == "" {
